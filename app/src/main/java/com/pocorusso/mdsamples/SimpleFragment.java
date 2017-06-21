8605ae33fa0b75ff9109dfba4c5a80187cdadccc
pocorusso <pocorusso@gmail.com>
2017-06-21 17:18:59 -0500

    init

---
+++ app/src/main/java/com/pocorusso/mdsamples/SimpleFragment.java
@@ -0,0 +1,18 @@
+package com.pocorusso.mdsamples;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SimpleFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_simple, container, false);
+        return v;
+    }
+}
